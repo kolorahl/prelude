3ff228d544386a80473a112a8835366fe70d5bad
Kolo Rahl <kolorahl@gmail.com>
2018-09-11 12:54:18 -0700

    Initial personal changes

---
+++ personal/options.el
@@ -0,0 +1,6 @@
+(prelude-require-packages '(evil alchemist))
+(setq prelude-guru nil)
+(require 'evil)
+(evil-mode 1)
+(set-face-attribute 'default nil :font "Source Code Pro-14")
+(global-hl-line-mode 0)
